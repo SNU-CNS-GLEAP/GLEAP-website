17379f72f7745593e1ef18238af9878b84cf46cb
Octo Moon <octahedron00@gmail.com>
2026-08-13 17:07:30 +0900

    file add + routing default to 'us'

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["ko", "en"],
-  defaultLocale: "ko",
+  defaultLocale: "en",
 });
 
 export type Locale = (typeof routing.locales)[number];
